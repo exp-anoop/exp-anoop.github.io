6042249681f41a6107500a82b6752a01ee74d05f
Anoop P R <anoopr@qburst.com>
2019-01-29 15:58:15 +0900

    Inital files

---
+++ precache-manifest.92cd7424a094484cac83dc4d9247c41c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb144a1131ef15331d3a",
+    "url": "/static/js/main.eb144a11.chunk.js"
+  },
+  {
+    "revision": "f5ec3a8a4b61fe34a5a9",
+    "url": "/static/js/1.f5ec3a8a.chunk.js"
+  },
+  {
+    "revision": "eb144a1131ef15331d3a",
+    "url": "/static/css/main.2f1ee9ec.chunk.css"
+  },
+  {
+    "revision": "f5ec3a8a4b61fe34a5a9",
+    "url": "/static/css/1.d3924b03.chunk.css"
+  },
+  {
+    "revision": "49fc11fd1801a4f3a82679d836c36f77",
+    "url": "/index.html"
+  }
+];
